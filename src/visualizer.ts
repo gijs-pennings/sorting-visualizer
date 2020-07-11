895451742f6f567a0db9441179933fbd300ca2cb
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2020-07-12 01:53:27 +0200

    Add general algorithm framework and reimplement bubble sort
    Sorting speed does not depend on FPS anymore.
    Added array.swap to prevent code repetition.

--- src/visualizer.ts
+++ src/visualizer.ts
@@ -1,48 +1,79 @@
+interface Array<T> {
+    shuffle(): void
+    swap(i: number, j: number): void
+}
+
 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-interface Array<T> { shuffle(): void }
 Array.prototype.shuffle = function() {
-    for (let i = this.length; i > 1; /* empty */) {
-        const j = Math.floor(Math.random() * i--)
-        const t = this[i]
-        this[i] = this[j]
-        this[j] = t
-    }
+    for (let i = this.length; i > 1; /* empty */)
+        this.swap(Math.floor(Math.random() * i--), i)
 }
 
-function step() {
+// no bounds checking!
+Array.prototype.swap = function(i, j) {
+    const t = this[i]
+    this[i] = this[j]
+    this[j] = t
+}
 
-    if (iBubble >= nBubble) {
-        iBubble = 0
-        nBubble--
-        if (nBubble <= 0) return
+abstract class Algorithm {
+
+    private lastTime: number | undefined
+
+    update(time: number) {
+        if (this.lastTime === undefined) {
+            this.lastTime = time
+            return
+        }
+
+        let steps = Math.round((time - this.lastTime) / msPerStep)
+        this.lastTime += steps * msPerStep
+
+        while (steps-- > 0) algorithm.step()
     }
 
-    iBubble++
+    abstract step(): void
 
-    if (array[iBubble - 1] > array[iBubble]) {
-        const t = array[iBubble]
-        array[iBubble] = array[iBubble - 1]
-        array[iBubble - 1] = t
+}
+
+// https://en.wikipedia.org/wiki/Bubble_sort#Optimizing_bubble_sort
+class BubbleSort extends Algorithm {
+
+    private i = 1
+    private n = barCount
+    private nNew = 0
+
+    step() {
+        if (this.n <= 1) return
+
+        if (this.i >= this.n) {
+            this.i = 1
+            this.n = this.nNew
+            this.nNew = 0
+        }
+
+        if (array[this.i - 1] > array[this.i]) {
+            array.swap(this.i - 1, this.i)
+            this.nNew = this.i
+        }
+
+        this.i++
     }
 
 }
 
-function draw() {
+function draw(time: number) {
+
+    algorithm.update(time)
 
     ctx.clearRect(0, 0, can.width, can.height)
-
-    for (let i = 0; i < barCount; i++) {
-        const n = array[i]
+    for (let i = 0; i < barCount; i++)
         ctx.fillRect(
             1.5 * i * barWidth,
             can.height,
             barWidth,
-            -can.height * (0.1 + 0.9 * n / (barCount - 1))
+            -can.height * (0.1 + 0.9 * array[i] / (barCount - 1))
         )
-    }
-
-    step()
-    step()
 
     window.requestAnimationFrame(draw)
 
@@ -51,14 +82,14 @@ function draw() {
 const can = document.querySelector('canvas')!
 const ctx = can.getContext('2d')!
 
-const barCount = 100 // (temp)
+// (temp) constants
+const barCount = 50
 const barWidth = can.width / (1.5 * barCount - 0.5)
+const msPerStep = 10 // => 100 steps per second
 
 const array: number[] = []
 for (let i = 0; i < barCount; i++) array[i] = i
 array.shuffle()
 
-let nBubble = barCount
-let iBubble = 0
-
+let algorithm = new BubbleSort()
 window.requestAnimationFrame(draw)
