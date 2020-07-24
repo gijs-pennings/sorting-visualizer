92c5c2726c912dc4022f5d54da22d0fb60f57da3
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2020-07-24 20:39:13 +0200

    Update from master

--- bundle.js
+++ bundle.js
@@ -8,6 +8,18 @@ Array.prototype.swap = function (i, j) {
     this[i] = this[j];
     this[j] = t;
 };
+function newElt(tag, options) {
+    const e = document.createElement(tag);
+    if (options === null || options === void 0 ? void 0 : options.class)
+        e.className = options.class;
+    if (options === null || options === void 0 ? void 0 : options.click)
+        e.onclick = options.click;
+    if (options === null || options === void 0 ? void 0 : options.id)
+        e.id = options.id;
+    if (options === null || options === void 0 ? void 0 : options.text)
+        e.textContent = options.text;
+    return e;
+}
 function randomInt(min, max) {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
@@ -28,37 +40,6 @@ function* bubble(a) {
         }
         accesses++;
         n = m;
-    }
-    return [accesses, comparisons];
-}
-function* counting(a) {
-    let accesses = 0;
-    let comparisons = 0;
-    accesses++;
-    let iMax = 0;
-    for (let i = 1; i < a.length; i++) {
-        accesses++, comparisons++;
-        yield [accesses, comparisons, [iMax, i], [], 'finding max'];
-        if (a[i] > a[iMax])
-            iMax = i;
-    }
-    const count = new Array(a[iMax] + 1).fill(0);
-    accesses += count.length;
-    for (let i = 0; i < a.length; i++) {
-        accesses += 2;
-        count[a[i]]++;
-        yield [accesses, comparisons, [], [i], 'counting'];
-    }
-    accesses += 2 * (count.length - 1);
-    for (let i = 1; i < count.length; i++)
-        count[i] += count[i - 1];
-    const aCopy = Array.from(a);
-    for (let i = a.length - 1; i >= 0; i--) {
-        accesses += 4;
-        const x = aCopy[i];
-        const j = (count[x]--) - 1;
-        a[j] = x;
-        yield [accesses, comparisons, [], [j], 'ordering'];
     }
     return [accesses, comparisons];
 }
@@ -265,80 +246,148 @@ function* shell(a) {
         }
     return [accesses, comparisons];
 }
-const barCount = Math.pow(2, 7);
+const barCount = Math.pow(2, 6);
 const msPerStep = 20;
+function addAlgorithm(algName, funName) {
+    const a = array.slice();
+    const f = window[funName];
+    const tuple = {
+        array: a,
+        function: f,
+        generator: f(a)
+    };
+    const header = newElt('div', { class: 'header' });
+    const canvas = newElt('canvas');
+    const vspace = newElt('span', { class: 'vspace24' });
+    header.appendChild(document.createTextNode(algName + ' ('));
+    header.appendChild(newElt('span', { id: 'accesses', text: '0' }));
+    header.appendChild(newElt('span', { class: 'hide-on-overflow', text: ' accesses' }));
+    header.appendChild(document.createTextNode(', '));
+    header.appendChild(newElt('span', { id: 'comparisons', text: '0' }));
+    header.appendChild(newElt('span', { class: 'hide-on-overflow', text: ' comparisons' }));
+    header.appendChild(document.createTextNode(')'));
+    header.appendChild(newElt('span', { class: 'hspace24' }));
+    header.appendChild(newElt('span', { class: 'material-icons close', click: function () {
+            algs.splice(algs.indexOf(tuple), 1);
+            header.remove();
+            canvas.remove();
+            vspace.remove();
+        }, text: 'close' }));
+    const divider = document.querySelector('#add');
+    divider.parentNode.insertBefore(header, divider);
+    divider.parentNode.insertBefore(canvas, divider);
+    divider.parentNode.insertBefore(vspace, divider);
+    setDimensions();
+    toggleDialog();
+    algs.push(tuple);
+}
 function calculateDimensions() {
-    can.width = Math.min(700, window.innerWidth);
-    let canWidthInner;
-    if ((can.width + 1) / barCount >= 3) {
-        barWidth = Math.floor((can.width + 1) / barCount);
-        barWidthInner = barWidth - 1;
-        canWidthInner = barCount * barWidth - 1;
+    dims.canWidth = Math.min(700, window.innerWidth);
+    if ((dims.canWidth + 1) / barCount >= 3) {
+        dims.barWidth = Math.floor((dims.canWidth + 1) / barCount);
+        dims.barWidthInner = dims.barWidth - 1;
+        dims.canWidthInner = barCount * dims.barWidth - 1;
     }
     else {
-        barWidth = Math.floor(can.width / barCount);
-        barWidthInner = barWidth;
-        canWidthInner = barCount * barWidth;
+        dims.barWidth = Math.floor(dims.canWidth / barCount);
+        dims.barWidthInner = dims.barWidth;
+        dims.canWidthInner = barCount * dims.barWidth;
     }
-    padding = Math.floor(0.5 * (can.width - canWidthInner));
-    can.height = Math.floor(0.5 * canWidthInner);
+    dims.canHeight = Math.floor(0.5 * dims.canWidthInner);
+    dims.canPadding = Math.floor(0.5 * (dims.canWidth - dims.canWidthInner));
+    setDimensions();
 }
-function reset() {
-    array.shuffle();
-    algorithm = quickHoare(array);
-    algorithmState = undefined;
-    lastTime = undefined;
+function drawAll() {
+    var _a, _b, _c, _d, _e;
+    for (let i = 0; i < algs.length; i++) {
+        const h = document.querySelectorAll('.header')[i];
+        const c = document.querySelectorAll('canvas')[i].getContext('2d');
+        const a = algs[i].array;
+        const s = (_a = algs[i].lastStep) === null || _a === void 0 ? void 0 : _a.value;
+        h.querySelector('#accesses').textContent = (_b = s === null || s === void 0 ? void 0 : s[0].toString()) !== null && _b !== void 0 ? _b : '0';
+        h.querySelector('#comparisons').textContent = (_c = s === null || s === void 0 ? void 0 : s[1].toString()) !== null && _c !== void 0 ? _c : '0';
+        c.clearRect(0, 0, dims.canWidth, dims.canHeight);
+        for (let j = 0; j < a.length; j++) {
+            c.fillStyle = '#111';
+            if ((_d = s === null || s === void 0 ? void 0 : s[3]) === null || _d === void 0 ? void 0 : _d.includes(j))
+                c.fillStyle = 'limegreen';
+            if ((_e = s === null || s === void 0 ? void 0 : s[2]) === null || _e === void 0 ? void 0 : _e.includes(j))
+                c.fillStyle = 'red';
+            c.fillRect(dims.canPadding + j * dims.barWidth, dims.canHeight, dims.barWidthInner, -dims.canHeight * (0.1 + 0.9 * a[j] / (a.length - 1)));
+        }
+    }
 }
-function update(time) {
-    var _a, _b, _c, _d;
-    window.requestAnimationFrame(update);
+function onFrame(time) {
+    var _a;
+    window.requestAnimationFrame(onFrame);
     if (playing) {
-        if (lastTime === undefined)
+        if (!lastTime)
             lastTime = time;
         let steps = Math.round((time - lastTime) / msPerStep);
         lastTime += steps * msPerStep;
-        while (steps-- > 0) {
-            algorithmState = algorithm.next();
-            if (algorithmState.done)
-                break;
-        }
+        for (const a of algs)
+            for (let i = 0; i < steps && !((_a = a.lastStep) === null || _a === void 0 ? void 0 : _a.done); i++)
+                a.lastStep = a.generator.next();
     }
-    ctx.clearRect(0, 0, can.width, can.height);
-    for (let i = 0; i < barCount; i++) {
-        ctx.fillStyle = 'black';
-        if ((_b = (_a = algorithmState === null || algorithmState === void 0 ? void 0 : algorithmState.value) === null || _a === void 0 ? void 0 : _a[3]) === null || _b === void 0 ? void 0 : _b.includes(i))
-            ctx.fillStyle = 'limegreen';
-        if ((_d = (_c = algorithmState === null || algorithmState === void 0 ? void 0 : algorithmState.value) === null || _c === void 0 ? void 0 : _c[2]) === null || _d === void 0 ? void 0 : _d.includes(i))
-            ctx.fillStyle = 'red';
-        ctx.fillRect(padding + i * barWidth, can.height, barWidthInner, -can.height * (0.1 + 0.9 * array[i] / (barCount - 1)));
-    }
+    drawAll();
 }
-const can = document.querySelector('canvas');
-const ctx = can.getContext('2d');
-let barWidth;
-let barWidthInner;
-let padding;
-window.addEventListener('resize', calculateDimensions);
-calculateDimensions();
-document.getElementById('reset').addEventListener('click', reset);
-let playing = false;
-document.querySelectorAll('#pause, #play').forEach(e => e.addEventListener('click', function () {
-    document.getElementById('pause').classList.toggle('hidden');
-    playing = document.getElementById('play').classList.toggle('hidden');
+function setDimensions() {
+    document.querySelectorAll('.header').forEach(h => {
+        h.style.width = dims.canWidthInner + 'px';
+        const hide = h.querySelectorAll('.hide-on-overflow');
+        hide.forEach(e => e.style.removeProperty('display'));
+        if (h.scrollWidth > h.clientWidth)
+            hide.forEach(e => e.style.display = 'none');
+    });
+    document.querySelectorAll('canvas').forEach(c => {
+        c.width = dims.canWidth;
+        c.height = dims.canHeight;
+    });
+}
+function toggleDialog() {
+    const d = document.querySelector('.dialog');
+    d.style.display = d.style.display ? '' : 'block';
+}
+function togglePlaying() {
     lastTime = undefined;
-}));
-document.getElementById('step').addEventListener('click', function () {
-    algorithmState = algorithm.next();
+    document.querySelector('#pause').classList.toggle('hidden');
+    playing = document.querySelector('#play').classList.toggle('hidden');
+}
+window.addEventListener('resize', calculateDimensions);
+document.querySelector('#reset').addEventListener('click', function () {
+    if (playing)
+        togglePlaying();
+    for (const a of algs) {
+        a.array = array.slice();
+        a.generator = a.function(a.array);
+        a.lastStep = undefined;
+    }
 });
-document.getElementById('skip').addEventListener('click', function () {
-    while (!(algorithmState === null || algorithmState === void 0 ? void 0 : algorithmState.done))
-        algorithmState = algorithm.next();
+document.querySelector('#pause').addEventListener('click', togglePlaying);
+document.querySelector('#play').addEventListener('click', togglePlaying);
+document.querySelector('#step').addEventListener('click', function () {
+    var _a;
+    for (const a of algs)
+        if (!((_a = a.lastStep) === null || _a === void 0 ? void 0 : _a.done))
+            a.lastStep = a.generator.next();
 });
+document.querySelector('#skip').addEventListener('click', function () {
+    var _a;
+    if (playing)
+        togglePlaying();
+    for (const a of algs)
+        while (!((_a = a.lastStep) === null || _a === void 0 ? void 0 : _a.done))
+            a.lastStep = a.generator.next();
+});
+document.querySelector('#add').addEventListener('click', toggleDialog);
+document.querySelector('.dialog > .close').addEventListener('click', toggleDialog);
+const algs = [];
 const array = [];
+const dims = {};
+let lastTime;
+let playing = false;
 for (let i = 0; i < barCount; i++)
     array[i] = i;
-let algorithm;
-let algorithmState;
-let lastTime;
-reset();
-window.requestAnimationFrame(update);
+array.shuffle();
+calculateDimensions();
+window.requestAnimationFrame(onFrame);
