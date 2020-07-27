cba102711ad8cfad244dbc25db2d43f26ce19153
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2020-07-27 23:42:04 +0200

    Reimplement 'ordinary' insertion sort and change direction
    The sorted part of the array during insertion sort is now on the right side, to make comparing with bubble sort etc. easier.

--- src/algorithms/insertion.ts
+++ src/algorithms/insertion.ts
@@ -1,55 +1,66 @@
 function* insertion(a: number[]): StepGenerator {
-
     let accesses = 0
     let comparisons = 0
 
-    for (let i = 1; i < a.length; i++) {
+    for (let i = a.length - 2; i >= 0; i--) {
         accesses++
 
-        // binary search for r = 1 + max { 0 ≤ j < i | a[j] ≤ a[i] }
-        let l = 0
-        let r = i
-        while (l < r) {
+        for (let j = i+1; j < a.length; j++) {
             accesses++, comparisons++
-            // The colors are not consistent with other algorithms: red is used
-            // for the bounds, while the compared element (i.e. a[i]) is green.
-            // Nonetheless, I think this paints the clearest picture.
-            yield [accesses, comparisons, [l, r], [i], 'searching']
-            const m = Math.floor((l + r) / 2)
-            if (a[m] > a[i])
-                r = m
-            else
-                l = m + 1
-        }
-
-        // move a[i] to a[r] without comparisons
-        for (let j = i; j > r; j--) {
-            accesses += 2
+            yield [accesses, comparisons, [j-1, j], [], undefined]
+            if (a[j-1] <= a[j]) break
+            accesses++
             a.swap(j-1, j)
-            yield [accesses, comparisons, [], [j-1], 'ordering']
         }
 
         accesses++
     }
 
     return [accesses, comparisons] as [number, number]
-
 }
-
 /*
 
-for i in 1 until a.length
+for i in a.length - 2 down to 0
     x ← a[i]
-    l ← 0
-    r ← i
-    while l < r
-        m ← ⌊(l + r) / 2⌋
-        if a[m] > x
-            r ← m
-        else
-            l ← m + 1
-    for j in i down to r+1
-        a[j] ← a[j-1]
-    a[r] ← x
+    j ← i+1
+    while j < a.length
+        y ← a[j]
+        if x ≤ y: break
+        a[j-1] ← y
+        j ← j+1
+    a[j-1] ← x
 
 */
+
+// optimized Java implementation: http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/java/util/TimSort.java#l273
+function* insertionBS(a: number[]): StepGenerator {
+    let accesses = 0
+    let comparisons = 0
+
+    for (let i = a.length - 2; i >= 0; i--) {
+        accesses++
+
+        // bounds are inclusive for prettier visualization
+        let l = i + 1
+        let r = a.length - 1
+
+        // binary search for l = min { i < j ≤ a.length | a[i] ≤ a[j] }
+        while (l <= r) {
+            const m = Math.ceil((l + r) / 2)
+            accesses++, comparisons++
+            yield [accesses, comparisons, [i, m], [l, r], undefined]
+            if (a[m] < a[i])
+                l = m + 1
+            else
+                r = m - 1
+        }
+
+        // move a[i] to a[l-1] without comparisons
+        accesses += 2 * (l - (i+1))
+        for (let j = i+1; j < l; j++) a.swap(j-1, j)
+
+        accesses++
+    }
+
+    return [accesses, comparisons] as [number, number]
+}
