ce1d381fce6bf89f0a7bef71bde60189fb45e64d
Gijs Pennings <49961459+gijs-pennings@users.noreply.github.com>
2020-07-28 00:33:57 +0200

    Move shaker sort to bubble.ts

--- src/algorithms/bubble.ts
+++ src/algorithms/bubble.ts
@@ -1,5 +1,4 @@
 function* bubble(a: number[]): StepGenerator {
-
     let accesses = 0
     let comparisons = 0
 
@@ -8,7 +7,7 @@ function* bubble(a: number[]): StepGenerator {
         let m = 0
         for (let i = 1; i < n; i++) {
             accesses++, comparisons++
-            yield [accesses, comparisons, [i-1, i], [n], undefined]
+            yield [accesses, comparisons, [i-1, i], [], undefined]
             accesses++
             if (a[i-1] > a[i]) {
                 a.swap(i-1, i)
@@ -20,9 +19,7 @@ function* bubble(a: number[]): StepGenerator {
     }
 
     return [accesses, comparisons] as [number, number]
-
 }
-
 /*
 
 n ← a.length
@@ -41,3 +38,46 @@ while n > 1
     n ← m
 
 */
+
+function* bubbleBi(a: number[]): StepGenerator {
+    let accesses = 0
+    let comparisons = 0
+
+    // inclusive
+    let iStart = 0
+    let iEnd = a.length - 1
+
+    while (iStart < iEnd /* i.e. there are >=2 unsorted elements */) {
+        let iNew = iStart
+
+        accesses++
+        for (let i = iStart + 1; i <= iEnd; i++) {
+            accesses++, comparisons++
+            yield [accesses, comparisons, [i-1, i], [], undefined]
+            accesses++
+            if (a[i-1] > a[i]) {
+                a.swap(i-1, i)
+                iNew = i
+            }
+        }
+        accesses++
+
+        iEnd = --iNew
+
+        accesses++
+        for (let i = iEnd; i > iStart; i--) {
+            accesses++, comparisons++
+            yield [accesses, comparisons, [i-1, i], [], undefined]
+            accesses++
+            if (a[i-1] > a[i]) {
+                a.swap(i-1, i)
+                iNew = i
+            }
+        }
+        accesses++
+
+        iStart = iNew
+    }
+
+    return [accesses, comparisons] as [number, number]
+}
